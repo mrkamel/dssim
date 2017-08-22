2b43cbb13cdf2d774cdb2ab057d718b1aeeda82f
Benjamin Vetter <vetter@plainpicture.de>
2017-08-22 21:25:21 +0200

    Tiny changes

--- lib/dssim.rb
+++ lib/dssim.rb
@@ -1,14 +1,17 @@
 
 require "dssim/version"
 require "securerandom"
+require "shellwords"
 require "fileutils"
 
 module Dssim
-  def self.optimize(path, dssim_path: "dssim", qualities: [50, 55, 60, 65, 70, 75, 80, 85, 90, 95], jpegoptim_options: "--strip-all", max: 0.0005)
+  def self.optimize(path, dssim_path: "dssim", qualities: [50, 55, 60, 65, 70, 75, 80, 85, 90, 95], jpegoptim_options: ["--strip-all"], max: 0.0005)
     begin
       png_path = "/tmp/#{SecureRandom.hex}.png"
 
-      system("convert", path, "-format", "png", png_path) || raise("convert error")
+      %x{ convert #{Shellwords.shellescape(path)} -format png #{png_path} }
+
+      raise("convert error") unless $?.success?
 
       qualities.each do |quality|
         random = SecureRandom.hex
@@ -19,8 +22,13 @@ module Dssim
         begin
           FileUtils.cp(path, destination_jpg)
 
-          system("jpegoptim #{jpegoptim_options} -m #{quality} #{destination_jpg} &> /dev/null") || raise("jpegoptim error")
-          system("convert #{destination_jpg} -format png #{destination_png} &> /dev/null") || raise("convert error")
+          %x{ jpegoptim #{jpegoptim_options.map { |option| Shellwords.shellescape(option) }.join(" ")} -m#{quality} #{destination_jpg} }
+
+          raise("jpegoptim error") unless $?.success?
+
+          %x{ convert #{destination_jpg} -format png #{destination_png} }
+
+          raise("convert error") unless $?.success?
 
           res = %x{ #{dssim_path} #{png_path} #{destination_png} }
 
